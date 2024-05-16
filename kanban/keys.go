3299ff762aa7a04afa4b6ee48f6cdc56f709c28d
Matthew Hall <matt.hall@getweave.com>
2024-05-16 10:53:15 -0600

    Show help for each individual navigate/move command in full help.

--- kanban/keys.go
+++ kanban/keys.go
@@ -26,15 +26,19 @@ func DefaultKeyMap() KeyMap {
 		),
 		Left: key.NewBinding(
 			key.WithKeys("left", "h"),
+			key.WithHelp("←/h", "navigate left"),
 		),
 		Down: key.NewBinding(
 			key.WithKeys("down", "j"),
+			key.WithHelp("↓/j", "navigate down"),
 		),
 		Up: key.NewBinding(
 			key.WithKeys("up", "k"),
+			key.WithHelp("↑/k", "navigate up"),
 		),
 		Right: key.NewBinding(
 			key.WithKeys("right", "l"),
+			key.WithHelp("→/l", "navigate right"),
 		),
 
 		Move: key.NewBinding(
@@ -43,15 +47,19 @@ func DefaultKeyMap() KeyMap {
 		),
 		MoveLeft: key.NewBinding(
 			key.WithKeys("shift+left", "H"),
+			key.WithHelp("shift+←/h", "move left"),
 		),
 		MoveDown: key.NewBinding(
 			key.WithKeys("shift+down", "J"),
+			key.WithHelp("shift+↓/j", "move down"),
 		),
 		MoveUp: key.NewBinding(
 			key.WithKeys("shift+up", "K"),
+			key.WithHelp("shift+↑/k", "move up"),
 		),
 		MoveRight: key.NewBinding(
 			key.WithKeys("shift+right", "L"),
+			key.WithHelp("shift+→/l", "move right"),
 		),
 	}
 }
@@ -59,8 +67,16 @@ func DefaultKeyMap() KeyMap {
 func (m KeyMap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
 		{
-			m.Navigate,
-			m.Move,
+			m.Left,
+			m.Down,
+			m.Up,
+			m.Right,
+		},
+		{
+			m.MoveLeft,
+			m.MoveDown,
+			m.MoveUp,
+			m.MoveRight,
 		},
 	}
 }
